be521757341cadbd5d344eed3e316e39455484fd
LENOVO <LENOVO@DESKTOP-EC0CQJN>
2023-09-30 04:32:28 +0530

    commint of web project

---
+++ DemoServlet/src/main/java/test/DemoServlet.java
@@ -0,0 +1,56 @@
+package test;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		//out.println("<h1>html</h1>");
+		out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello, World Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("<p>This is a simple servlet example.</p>");
+        out.println("</body>");
+        out.println("</html>");
+		
+		
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
